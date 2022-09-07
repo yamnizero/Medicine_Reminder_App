772a67fd5ab6293130cdb716aa3ad6ffd6c1b3fb
mohammed jamal <yamni.zero@gmail.com>
2022-09-08 00:48:27 +0200

    notification and home

---
+++ android/app/src/main/kotlin/com/example/medicine_reminder_app2022/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.medicine_reminder_app2022
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:medicine_reminder_app2022/UI/home_page.dart';
+import 'package:medicine_reminder_app2022/UI/theme.dart';
+
+import 'services/theme_services.dart';
+
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+   await GetStorage.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: Themes.light,
+      darkTheme: Themes.dark,
+      themeMode: ThemeService().theme,
+      home:  const HomePage(),
+    );
+  }
+}
+
+
